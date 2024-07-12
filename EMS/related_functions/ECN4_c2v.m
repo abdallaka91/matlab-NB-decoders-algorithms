7faac3f9d56d11c97f315fe71b4bb0eeb9f7fc2d
Abdallah Abdallah <abdallah.91ka.com>
2024-07-12 11:26:16 -0600

    try to make c2v > v2c in the EMS

---
+++ EMS/related_functions/ECN4_c2v.m
@@ -0,0 +1,44 @@
+function V = ECN4_c2v(q, nm, nc, I, U, add_mat,comp_ECN) %        [m1,n1] = min(srt_llr);
+
+I_gf = I(1,:)+1;
+I_llr = I(2,:);
+U_gf = U(1,:)+1;
+U_llr = U(2,:);
+V = nan(2,nc);
+Mllr = zeros(nm*nm,1);
+Mgf = Mllr;
+k=0;
+for i = 1 : nm
+    for j = 1 : nm
+        k = k+1;
+        Mllr(k) = I_llr(i)+U_llr(j);
+        Mgf(k) = add_mat(I_gf(i), U_gf(j))+1;
+    end
+end
+
+V_gf = false(1,q);
+V_llr = zeros(1,q);
+
+[Mllr1,b] = sort(Mllr);
+Mgf1 = Mgf(b);
+
+k=1;
+for i = 1 : nm^2
+    if  (V_gf(Mgf1(i)))==false
+        V_gf(Mgf1(i)) = true;
+        V_llr(Mgf1(i)) = Mllr1(i);
+        k=k+1;
+    end
+    if k> nc
+        break;
+    end
+end
+V_gf1=find(V_gf);
+V_llr1 = V_llr(V_gf1);
+[V_llr1,b1] = sort(V_llr1);
+V_gf = V_gf1(b1)-1;
+
+    V(1,1:k-1) = V_gf;
+    V(2,1:k-1) = V_llr1;
+
+end
---
+++ UTAH/NGDBFoctave/decodeGDBFvec.m
@@ -0,0 +1,13 @@
+function [d, failed, S, Sb, E]  = decodeGDBFvec(y, H, N, R, M, T, w, theta, nsigma)
+
+d = sign(y);
+l=0;
+while l<T
+    Sb = mod((0.5*(1-d))*H',2);
+  S = 1-2*Sb;
+  E = d.*y + w*S*H + nsigma*randn(R,N);
+  flipdx = find(E<theta);
+  d(flipdx) = -d(flipdx);
+  failed=sum(Sb');
+  l=l+1;
+end
