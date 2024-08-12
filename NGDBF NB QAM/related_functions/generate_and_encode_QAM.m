12aaeda82d00c0cd21dc45cd007d5b225e699186
abdallaka91 <abdallah.91ka@gmail.com>
2024-08-12 23:35:39 +0200

    update NGDBF with QAM

--- NGDBF NB QAM/related_functions/generate_and_encode_QAM.m
+++ NGDBF NB QAM/related_functions/generate_and_encode_QAM.m
@@ -11,9 +11,8 @@ info_seq = ZERO*randi([0 q-1], 1, K);
 code_seq_gray = gf_mat_mul(info_seq_gray,G, add_mat, mul_mat);
 code_seq = gray_labels(code_seq_gray+1);
 
-valid_symdrom = gf_mat_mul(code_seq,h', add_mat, mul_mat);
+valid_symdrom = gf_mat_mul(gray_labels(code_seq+1),h', add_mat, mul_mat);
 
-info_seq_comp = constl(info_seq_gray+1);
 code_seq_comp = constl(code_seq_gray+1);
 
 
