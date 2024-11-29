1f44c1a2044d1fc9168eb61108bb3a496ba3cc2b
abdallaka91 <154563304+abdallaka91@users.noreply.github.com>
2024-11-29 19:07:55 +0100

    FER MVSF Vs FER AMA-MVSF Vs iterations

--- Copy_of_AMA MVSF/call_AMA_MVSF.m
+++ Copy_of_AMA MVSF/call_AMA_MVSF.m
@@ -22,8 +22,8 @@ max_err_cnt1 = 60; % at low Eb_No(<Eb_No_thrshld)
 max_err_cnt2 = 60; %at high Eb_No
 Eb_No_thrshld = 3;
 max_gen = 2e5;
-max_iter = 96;
-max_attempt = 1;
+max_iter = 16;
+max_attempt = 2;
 ebn0 = 3.0;%1.4:0.2:4.2; %dB
 
 
@@ -159,7 +159,6 @@ for i0 = 1 : snr_cnt
     while FER(i0) < max_err_cnt && gen_seq_cnt(i0)<max_gen
         BER_iter0 = zeros(parforN, max_attempt*max_iter);
         FER_iter0 = zeros(parforN, max_attempt*max_iter);
-        gen_iter0 = zeros(parforN, max_attempt*max_iter);
         parfor j = 1 : parforN
             if ZERO
                 [info_seq, code_seq, valid_symdrom, y_bin] = generate_and_encode(ZERO, h,G, add_mat, mul_mat, p);
@@ -176,9 +175,9 @@ for i0 = 1 : snr_cnt
 %             HD1 = HD1'-1;
 %             nes = sum(HD1~=code_seq);
 
-            [iters, dec_seq, success_dec, FER_iter0(j,:), BER_iter0(j,:), gen_iter0(j,:)] = ...
+            [iters, dec_seq, success_dec, FER_iter0(j,:), BER_iter0(j,:)] = ...
                 presorted_MVSF_with_rinfrc(LLR_2,HD1, max_iter, mul_mat, add_mat, div_mat,...
-                h,str_cn_vn, dc, dev_lsts, nm, v_weights, max_attempt,code_seq, FER_iter0(j,:), BER_iter0(j,:), gen_iter0(j,:));
+                h,str_cn_vn, dc, dev_lsts, nm, v_weights, max_attempt,code_seq, FER_iter0(j,:), BER_iter0(j,:) );
             needed_iters_(j) = iters;
 
             iter_cnt_ = iter_cnt_ + iters;
@@ -194,10 +193,11 @@ for i0 = 1 : snr_cnt
         end
         s1b = sum(BER_iter0, 1);
         s1f = sum(FER_iter0, 1);
-        s1g = sum(gen_iter0, 1);
 
         BER_iter = BER_iter + s1b;
         FER_iter = FER_iter + s1f;
+        gen_iter = gen_iter + parforN;
+
         needed_iters(KK+1:KK+parforN,i0) = needed_iters_;
         KK=KK+parforN;
         SER(i0) = SER_;
@@ -218,11 +218,12 @@ for i0 = 1 : snr_cnt
         fprintf(msg)
 
         msgs  = details_in_lines(ebn0, FER,BER, SER, gen_seq_cnt, K,p, aver_iter, conf_detail, report_fle_nme, save_rslt);
+        BER_iter1 = BER_iter./(gen_iter*N*p);
+        FER_iter1 = FER_iter./gen_iter;
         if save_rslt
             save(report_fle_nme+'.mat');
         end
     end
-    s1b1 = s1b./(s1g*N*p);
-    s1f1 = s1f./s1g;
+
     
 end
